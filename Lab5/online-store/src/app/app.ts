11aa321c4b3a91a9a2aa72fc630d721e00b348cf
Almas Magrupov <magrupovalmas06@gmail.com>
2026-02-24 10:18:05 +0500

    Final organization: Lab 4 cleaned and Lab 5 structured

--- Lab5/online-store/src/app/app.ts
+++ Lab5/online-store/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ProductListComponent], 
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('online-store');
+export class AppComponent {
+  title = 'online-store';
 }
